f3e008e574a7e5b18e481de7d3c6074f4af8a538
viktor ajayi <victorajayidamilare@gmail.com>
2025-05-08 17:12:16 +0100

    dd

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,8 @@
+import "../globals.css";
+import React from "react";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default RootLayout;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './app/**/*.{ts,tsx}',      // For App Router
+    './components/**/*.{ts,tsx}',
+    './pages/**/*.{ts,tsx}',    // If you use /pages too
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
